edeaf340503b755e0e44fe08d50b4014f8b16634
Delyan Angelov <delian66@gmail.com>
2019-08-14 12:34:36 +0300

    Cleanup indentation of tests.

--- vini_extended_test.v
+++ vini_extended_test.v
@@ -3,6 +3,5 @@ import spytheman.vini
 
 fn test_extended_1() {
 	println('hello from test_extended_1')
-  assert true
+	assert true
 }
-
--- vini_test.v
+++ vini_test.v
@@ -3,10 +3,10 @@ import spytheman.vini
 
 fn test_default_parser() {
 	println('hello from test_default_parser')
-  assert true
+	assert true
 }
 
 fn test_colon_parser() {
 	println('hello from test_colon_parser')
-  assert true
+	assert true
 }
