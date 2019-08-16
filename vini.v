32e935172f9a4ecb0f4b111108c53507537d1075
Delyan Angelov <delian66@gmail.com>
2019-08-16 17:27:21 +0300

    Working parsing of section names and comments from INI files.

--- vini.v
+++ vini.v
@@ -3,6 +3,7 @@ module vini
 
 import os
 
+
 struct IniConfig {
 	assign       byte
 	comment      byte
@@ -42,12 +43,14 @@ struct IniReader  {
 mut:
 	text string
 	pos int
+	line int
+	col int
 	csection string
 	output IniResults
 }
 
 pub fn (r IniReader) str() string {
-	return 'IniReader{ pos: ${r.pos},  csection: "${r.csection}",  config: { assign: ${r.config.assign}, comment: ${r.config.comment},  comment_semi: ${r.config.comment_semi} } }'
+	return 'IniReader{ line: ${r.line}, pos: ${r.pos},  csection: "${r.csection}",  config: { assign: ${r.config.assign}, comment: ${r.config.comment},  comment_semi: ${r.config.comment_semi} } }'
 }
 
 pub fn new_ini_reader(text string) IniReader {
@@ -55,6 +58,7 @@ pub fn new_ini_reader(text string) IniReader {
 }
 pub fn new_ini_reader_with_config(text string, cfg IniConfig) IniReader {
 	return IniReader{
+		line: 1
 		text: text
 		config: cfg
 		csection: 'global'
@@ -72,14 +76,67 @@ pub fn parse_ini_file_with_config(path string, cfg IniConfig) IniReader? {
 }
 
 //////////////////////////////////////////////////////////////
+fn (r mut IniReader) comment_till_end_of_line() string {
+	mut c := ` `
+	mut cstart := r.pos
+	for {
+		c = r.text[ r.pos ]
+		if c == `\r` || c == `\n` {
+			break
+		}
+		r.pos++
+	}
+	return r.text.substr( cstart, r.pos )
+}
+
+fn (r mut IniReader) new_section_name() string {
+	mut c := ` `
+	r.pos++
+	mut cstart := r.pos
+	for {
+		c = r.text[ r.pos ]
+		if c == `]` { break	}
+		r.pos++
+	}
+	sname := r.text.substr( cstart, r.pos )
+	r.pos++
+	return sname
+}
+
+fn (r mut IniReader) skip_line_ends(skipchars int) {
+	r.line++
+	r.pos += skipchars
+	r.col = 0	
+}
 
 pub fn (r mut IniReader) parse() IniResults {
-	r.output = IniResults{}
 	mut c := ` `
+	mut n := ` `
+	println('len: $r.text.len ')
 	for {
-		if r.text.len <= r.pos { break }
+		r.col++
+		if r.pos >= r.text.len { break }
 		c = r.text[ r.pos ]
-		//println('len: $r.text.len | pos: ${r.pos:4d} | "$c" ')
+		n = if r.pos + 1 < r.text.len { r.text[ r.pos + 1 ] }else{` `}
+		if c == `\r` && n == `\n` {
+			r.skip_line_ends(2)
+			continue
+		}
+		if c == `\n` {
+			r.skip_line_ends(1)
+			continue
+		}
+//		println('line: ${r.line:4d} | col: ${r.col:3d} | pos: ${r.pos:4d} | c:"${c.str()}" | n:"${n.str()}" ')
+		if c == r.config.comment || c == r.config.comment_semi {
+			comment := r.comment_till_end_of_line()
+			println( '// Found comment: "$comment" ' )
+			continue
+		}
+		if c == `[`  {
+			new_section_name := r.new_section_name()
+			println( '// Found new section: "$new_section_name" ')
+			continue
+		}
 		r.pos++
 	}
 	return r.output
